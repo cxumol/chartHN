a2e32be7d7b00d7af14f286c28dba728cea63fbe
cxumol <cxumol@gmail.com>
2024-11-26 23:37:32 +0000

    add run_batch.sh

---
+++ run_batch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set default values
+per_page=1
+page=1
+
+# Check if arguments are provided and are numbers
+if [ $# -ge 1 ] && [[ "$1" =~ ^[0-9]+$ ]]; then
+  per_page=$1
+fi
+
+if [ $# -ge 2 ] && [[ "$2" =~ ^[0-9]+$ ]]; then
+  page=$2
+fi
+
+mkdir -p ../chartHN_tmp/data
+git checkout data
+rsync -av data/ ../chartHN_tmp/data
+git checkout master
+cp -ru ../chartHN_tmp/* ./
+
+node --experimental-default-type=module --env-file=.env batch.js $per_page $page
+
+bash git_to_data_branch.sh
