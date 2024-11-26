4a5a079a7c27ed75d78e3ae0417b55e0062ddf27
cxumol <cxumol@gmail.com>
2024-11-26 06:54:31 +0000

    add git_to_data_branch.sh

---
+++ git_to_data_branch.sh
@@ -0,0 +1,12 @@
+mkdir -p ../chartHN_tmp
+mv .env ../chartHN_tmp/ || echo "no .env"
+mv data ../chartHN_tmp/
+
+
+git checkout data
+git rm -r --cached .
+# Move contents of data to the root, handling subdirectories correctly. -a preserves attributes
+cp -r ../chartHN_tmp/data ./
+git add .
+git commit -m "manual update $(date +%Y-%m-%d)"
+git push
