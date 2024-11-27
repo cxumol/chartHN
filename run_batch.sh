13fa2155ce3b8956306f4b31474fea06f3c4baad
cxumol <cxumol@gmail.com>
2024-11-27 02:21:16 +0000

    update sh

--- run_batch.sh
+++ run_batch.sh
@@ -17,7 +17,7 @@ mkdir -p ../chartHN_tmp/data
 git checkout data
 rsync -av data/ ../chartHN_tmp/data
 git checkout master
-rsync -au ../chartHN_tmp/. ./
+rsync -au ../chartHN_tmp/. ./ # dot. to cp hidden files
 
 node --experimental-default-type=module --env-file=.env batch.js $per_page $page
 
