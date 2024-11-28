6fb3c0c3fa80c9cf16cff163788f3aea1945798e
cxumol <cxumol@gmail.com>
2024-11-28 10:43:34 +0000

    rm redundant data dir

--- git_to_data_branch.sh
+++ git_to_data_branch.sh
@@ -1,13 +1,13 @@
 mkdir -p ../chartHN_tmp
 mv .env ../chartHN_tmp/ || echo "no .env"
 # mv data ../chartHN_tmp/
-rsync -av data/ ../chartHN_tmp/data
+rsync -av data/ ../chartHN_tmp/data/
 
 
 git checkout data
 git rm -r --cached .
 # Move contents of data to the root, handling subdirectories correctly. -a preserves attributes
-rsync -au ../chartHN_tmp/data ./
+rsync -au ../chartHN_tmp/data/ ./
 git add .
 git commit -m "update $(date +%Y-%m-%d)"
 git push
