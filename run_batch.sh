55cd590666fdb5c808cfcc6cfaa77c86dc689c5e
cxumol <cxumol@gmail.com>
2024-12-13 05:44:34 +0000

    sync sh from ghac

--- run_batch.sh
+++ run_batch.sh
@@ -15,7 +15,7 @@ fi
 
 mkdir -p ../chartHN_tmp/data
 git checkout data
-rsync -av ./* ../chartHN_tmp/data
+rsync -a --exclude='.git' ./* ../chartHN_tmp/data/
 git checkout master
 rsync -au ../chartHN_tmp/. ./ # dot. to cp hidden files
 
